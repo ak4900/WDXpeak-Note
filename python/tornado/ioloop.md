111255c7ed305755b437d1f6f87ae8eb13eac719
wdxtub <dacrocodilee@gmail.com>
2015-03-19 16:28:01 +0800

    clean up

--- python/tornado/ioloop.md
+++
@@ -1,3 +0,0 @@
-
-<tornado.platform.kqueue.KQueueIOLoop object at 0x1014cbd10>
-<tornado.platform.epoll.EPollIOLoop object at 0x120fa90>
--- wkk/flask/schema.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists entires;
-
-create table entries(
-    id integer primary key autoincrement,
-    title string not null,
-    text string not null
-);
